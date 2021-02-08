20c5119b99d5d85c8f8d060302ce6d35f121b1b7
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-08 14:10:25 -0700

    charAt

--- app/strings.js
+++ app/strings.js
@@ -1,10 +1,12 @@
 // Write a function that returns the character from a string at a given position
+var word = "i"
+var pos = word.charAt(0)
 function letterAtPosition(str, position) {
-
+    console.log(pos)
+    return (pos)
 }
-
-// Write a function that will join two strings with an optional seperating character 
-function concat(str1, str2, seperator = " ") {
+// Write a function that will join two strings with an optional separating character 
+function concat(str1, str2, separator = " ") {
 }
 
 // Write a function that will capitalize the letter at a given position and return it
