452e272f3c6b3eb92201f5af6306979a665101c2
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-08 14:27:24 -0700

    strings

--- app/strings.js
+++ app/strings.js
@@ -1,14 +1,25 @@
 // Write a function that returns the character from a string at a given position
-var word = "i"
-var pos = word.charAt(0)
+let word= "i"
+let pos = word.charAt(0)
 function letterAtPosition(str, position) {
-    console.log(pos)
     return (pos)
 }
+
+
 // Write a function that will join two strings with an optional separating character 
+var str1 = "Javascript "
+var str2 = "awesome!"
+var sep = "is "
+var res = str1.concat(sep, str2)
 function concat(str1, str2, separator = " ") {
+    return (res)
 }
 
+
 // Write a function that will capitalize the letter at a given position and return it
+var letter = "n"
+var up = letter.toUpperCase(0)
 function capitalizer(str, position) {
+    console.log(up)
+    return(up)
 }
