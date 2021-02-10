bc88908f43e9e9c5fa5978d1b3b5e9d953f50aad
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-09 23:04:39 -0700

    fixed numbers

--- app/numbers.js
+++ app/numbers.js
@@ -1,34 +1,30 @@
 // Write a function that uses the addition operator to add two numbers together
-var x = 2
-var y = 3
 function add(x, y) {
     return (x+y)
 }
 
 // Write a function that returns the Cubed value of x
-var x = 3
-var cube = Math.pow(x, 3)
 function cubed(x) {
-    return (cube)
+    x = Math.pow(x, 3)
+    return (x)
 }
 
 // Write a function that will divide two numbers, and will format the returned value
 // as a whole number. (No Decimals)
-var x = 8
-var y = 4
-var z = Math.floor(x / y)
+
+
 function divideWholey(x, y) {
+    var z = Math.floor(x / y)
     return (z)
 }
 
 // Write a function that takes in a number and determines if it is even. 
 // It should return a boolean value - true if it is even, false if it is odd.
-var x = 4
 function isEven(num) {
-    if (x % 2 == 0){
+    if (num % 2 == 0){
         return(true)
     }
-    else if (x % 2 == 1){
+    else if (num % 2 == 1){
         return (false)
     }
 }
@@ -37,8 +33,6 @@ function isEven(num) {
 // Given 2 numbers, generate a random number within that range. 
 // Make it inclusive so that the starting and ending numbers are part 
 // of the range.HINT ** Utilize Javascript's Math Class (To the Google!)
-var num1 = 2
-var num2 = 10
 function numberGenerator(num1, num2) {
     return Math.floor(Math.random() * (num2 - num1) + num1)
 }
